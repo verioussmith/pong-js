a8cf9db7fa69792556c0bd7bf8abe1f979d45273
Verious Smith III <verious.smith@philoveracity.com>
2017-09-26 01:07:33 -0700

    add styles, add canvas ball

--- pong.js
+++ pong.js
@@ -1,5 +1,10 @@
 const canvas = document.getElementById('pong');
 const c = canvas.getContext('2d');
 
+// Draw the Canvas 
 c.fillStyle = '#000';
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+// Draw the Ball
+c.fillStyle = '#fff';
+c.fillRect(0, 0, 10, 10);
