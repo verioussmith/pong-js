65c2b381c6b3ee446f0d0f1ea31de2661c6022de
Verious Smith III <verious.smith@philoveracity.com>
2017-09-26 01:24:57 -0700

    Add several classes for the ball. Update md file

---
+++ README.md
@@ -0,0 +1,4 @@
+### Javascript Pong
+
+- Follow along tutorial to experiment with what it is like to create the classic pong game in Javascript.
+- Tutorial is listed [here on YouTube](https://www.youtube.com/watch?v=ju09womACpQ)
--- pong.js
+++ pong.js
@@ -1,5 +1,36 @@
+class Vec
+{
+	constructor(x = 0, y = 0)
+	{
+		this.x = x
+		this.y = y
+	}
+}
+  
+class Rect
+{
+	constructor(w, h)
+	{
+		this.pos = new Vec;
+		this.size = new Vec(w, h);
+	}
+}
+
+class Ball extends Rect
+{
+	constructor()
+	{
+		super(10, 10);
+		this.vel = new Vec;
+	}
+}
+
 const canvas = document.getElementById('pong');
 const c = canvas.getContext('2d');
+
+const ball = new Ball;
+console.log(Ball);
+
 
 // Draw the Canvas 
 c.fillStyle = '#000';
