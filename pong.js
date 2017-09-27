504eff527a300ff7eec7402a5b9bf86842783cd3
Verious Smith III <verious.smith@philoveracity.com>
2017-09-26 23:34:15 -0700

    Add update function, move code to update scope

--- pong.js
+++ pong.js
@@ -29,13 +29,43 @@ const canvas = document.getElementById('pong');
 const c = canvas.getContext('2d');
 
 const ball = new Ball;
-console.log(Ball);
+ball.pos.x = 100;
+ball.pos.y = 50;
+
+ball.vel.x = 100;
+ball.vel.y = 100;
+
+let lastTime;
+function callback(millis) {
+	if (lastTime) {
+		update((millis - lastTime) / 1000);
+	}
+	lastTime = millis;
+	requestAnimationFrame(callback);
+}
 
 
-// Draw the Canvas 
-c.fillStyle = '#000';
-c.fillRect(0, 0, canvas.width, canvas.height);
+function update(dt) {
+	ball.pos.x += ball.vel.x * dt;
+	ball.pos.y += ball.vel.y * dt;
 
-// Draw the Ball
-c.fillStyle = '#fff';
-c.fillRect(0, 0, 10, 10);
+	if (ball.pos.x < 0 || ball.pos.x > canvas.width) {
+		ball.vel.x = - ball.vel.x;
+	}
+
+	if (ball.pos.y < 0 || ball.pos.y > canvas.height) {
+		ball.vel.y = - ball.vel.y;
+	}
+
+
+	// Draw the Canvas 
+	c.fillStyle = '#000';
+	c.fillRect(0, 0, canvas.width, canvas.height);
+
+	// Draw the Ball
+	c.fillStyle = '#fff';
+	c.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+}
+
+callback();
+
